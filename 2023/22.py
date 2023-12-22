9e961f97591cac4c0b2a903f285c9dda28e77c53
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2023-12-22 22:49:22 +0500

    Add day 22

--- 2023/22.py
+++ 2023/22.py
@@ -17,37 +17,44 @@ def solution():
         bricks.append((x1, y1, z1, x2, y2, z2))
 
     bricks = sorted(bricks, key=lambda b: (b[2]))
-    
-    def free_falling(bricks):
+
+    def free_falling(bricks, skip=-1):
+        res = {}
         for i in range(len(bricks)):
+            if i == skip:
+                continue
             x1, y1, z1, x2, y2, z2 = bricks[i]
             height, hpz2 = z2-z1, 0
-            for j in range(i):
-                px1, py1, pz1, px2, py2, pz2 = bricks[j]
+            for j in res:
+                px1, py1, pz1, px2, py2, pz2 = res[j]
                 if pz2 <= hpz2:
                     continue
                 if ((x1 <= px1 <= x2 or x1 <= px2 <= x2) or (px1 <= x1 <= px2 or px1 <= x2 <= px2)) and ((y1 <= py1 <= y2 or y1 <= py2 <= y2) or (py1 <= y1 <= py2 or py1 <= y2 <= py2)):
                     if pz2 > hpz2:
                         hpz2 = pz2
-                    
             if hpz2:
                 z1, z2 = hpz2+1, hpz2+1+height
-                bricks[i] = (x1, y1, z1, x2, y2, z2)
+                res[i] = (x1, y1, z1, x2, y2, z2)
             else:
-                bricks[i] = (x1, y1, 1, x2, y2, height+1)
-        return bricks
+                res[i] = (x1, y1, 1, x2, y2, height+1)
+        return res
+    
 
-    bricks = free_falling(bricks)
+    bricks_dict = free_falling(bricks)
     res, part2 = 0, 0
     for i in tqdm(range(len(bricks))):
-        temp = bricks[:i]+bricks[i+1:]
-        s = set(temp)
-        new_bricks = free_falling(temp)
-        diff = len(s.difference(set(new_bricks)))
-        if diff == 0: res += 1
+        d = free_falling(bricks, i)
+        diff = 0
+        for j in d:
+            if d[j] != bricks_dict[j]:
+                diff += 1
+        
         part2 += diff
-            
+        if not diff:
+            res += 1
+
     return res, part2
+            
 
 for part2 in [False]:
     print(solution())
