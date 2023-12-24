d656d4993ecb6bbcbe23e534187328d343086954
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2023-12-24 12:37:18 +0500

    Add day 24

--- 2023/24.py
+++ 2023/24.py
@@ -1,5 +1,5 @@
 import sys
-from tqdm import tqdm
+import z3
 
 
 filename = 'input.txt' if len(sys.argv) == 1 else sys.argv[1]
@@ -11,16 +11,16 @@ with open(filename, 'r') as file:
 def part_one():
     funcs, start, end = [], 200000000000000, 400000000000000
     for line in lines:
-        points, velocity = line.split(' @ ')
-        px, py, pz = [int(point) for point in points.split(', ')]
-        vx, vy, vz = [int(vel) for vel in velocity.split(', ')]
+        pos, vel = line.split(' @ ')
+        px, py, _ = [int(point) for point in pos.split(', ')]
+        vx, vy, _ = [int(vel) for vel in vel.split(', ')]
         slope = vy / vx
         intercept = py - slope * px
         dir = int.__lt__ if vx < 0 else int.__gt__
         funcs.append((slope, intercept, px, dir))
         
     res = 0
-    for i in tqdm(range(len(funcs))):
+    for i in range(len(funcs)):
         cx, cy, scx, scdir = funcs[i]
         for j in range(i + 1, len(funcs)):
             dx, dy, sdx, sddir = funcs[j]
@@ -31,7 +31,7 @@ def part_one():
             x = (dy - cy) / (cx - dx)
             y = cx * x + cy
             
-            # check if they don't cross before the snowing
+            # check if they only cross in the future
             if not scdir(int(x), int(scx)) or not sddir(int(x), int(sdx)):
                 continue
             
@@ -42,8 +42,26 @@ def part_one():
 
 
 def part_two():
-    pass
+    hails = {}
+    for line in lines:
+        pos, vel = line.split(' @ ')
+        px, py, pz = [float(point) for point in pos.split(', ')]
+        vx, vy, vz = [float(vel) for vel in vel.split(', ')]
+        hails[(px, py, pz)] = (vx, vy, vz)
+    
+    x, y, z, vx, vy, vz = z3.Reals('x y z vx vy vz')
+    s = z3.Solver()
 
+    for (px, py, pz), (pvx, pvy, pvz) in hails.items():
+        t = z3.Real(f"t_{(px, py, pz)}")
+        s.add(px + pvx * t == x + vx * t)
+        s.add(py + pvy * t == y + vy * t)
+        s.add(pz + pvz * t == z + vz * t)
+    
+    s.check()
+    m = s.model()
+    return eval(str(m[x] + m[y] + m[z]))
+        
 
 print(part_one())
 print(part_two())
